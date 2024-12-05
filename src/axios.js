74d60e20e82494132c1e4ec8097d7d93947b73bb
Anshu singh <idontknowanything144@gmail.com>
2024-12-05 16:01:01 +0530

    integrating with production backend

--- src/axios.js
+++ src/axios.js
@@ -3,16 +3,11 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    // baseURL: 'https://online-voting-system-sigma-sable.vercel.app/api',
-    // headers: {
-    //     'Content-Type': 'application/json',
-    // },
     baseURL: 'https://ovs-backend.vercel.app',
-//   withCredentials: true,  // Ensure credentials are always sent
-  headers: {
-    'Content-Type': 'application/json',
-  },
+    withCredentials: true,  // Ensure credentials are always sent
+    headers: {
+        'Content-Type': 'application/json',
+    },
 });
 
 export default instance;
-//
