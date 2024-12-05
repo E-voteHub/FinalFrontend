e22f7b53482eb59077f433a59052c56c2c607612
Anshu singh <idontknowanything144@gmail.com>
2024-12-05 13:58:14 +0530

    updated ui and Api calls 2

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';  // Correct import
-import Navbar from './navbar.jsx';
+import Navbar from './Navbar.jsx';
 import Login from './Login.jsx';
 import Register from './Register.jsx';
 import { Provider, useSelector, useDispatch } from 'react-redux';
