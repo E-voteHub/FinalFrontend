bfc580d88769aa16b13152ad2636d29e9611bea0
Anshu singh <idontknowanything144@gmail.com>
2024-12-05 21:24:48 +0530

    production update

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3000',
+    baseURL: 'https://ovs-backend.vercel.app',
     //withCredentials: true,  // Ensure credentials are always sent
     headers: {
         'Content-Type': 'application/json',
