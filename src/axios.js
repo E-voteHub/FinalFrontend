88d4f862308c11038821ff10660e3b94613588ba
Anshu singh <idontknowanything144@gmail.com>
2024-12-08 23:17:28 +0530

    did something

--- src/axios.js
+++ src/axios.js
@@ -3,7 +3,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://ovs-backend.vercel.app',
+    baseURL: 'https://ovs-backend.vercel.app/',
     //withCredentials: true,  // Ensure credentials are always sent
     headers: {
         'Content-Type': 'application/json', // mewo
